ccb3b1924d55ad335cb477eefac6ae23a715eb08
Eric Templeton <erictempleton1@users.noreply.github.com>
2020-11-11 01:54:02 +0000

    working on db stuff

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import Depends, FastAPI, Header, HTTPException
-from .routers import users
+from .database import connection
+from app import routers, crud, schemas, models
 
+models.user.connection.Base.metadata.create_all(bind=connection.engine)
 app = FastAPI()
+
+
+def get_db():
+    db = connection.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 async def get_token_header(x_token: str = Header(...)):
@@ -9,7 +19,7 @@ async def get_token_header(x_token: str = Header(...)):
         raise HTTPException(status_code=400, detail='X-Token header invalid')
 
 app.include_router(
-    users.router,
+    routers.users.router,
     dependencies=[Depends(get_token_header)],
     responses={404: {'description': 'Not found'}}
 )
